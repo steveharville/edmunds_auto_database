2b5465f57bc7549cac93f7b1bb0f7a2b746492ff
Steve Harville <steve.harville@gmail.com>
2018-05-01 11:11:59 -0400

    Create environment.sh

---
+++ environment.sh
@@ -0,0 +1,7 @@
+export LD_LIBRARY_PATH=/home/oracle/app/oracle/product/12.1.0/dbhome_1/lib/
+export ORACLE_HOME=/home/oracle/app/oracle/product/12.1.0/dbhome_1
+export NLS_LANG=.UTF8
+export CX_ORACLE_USERNAME=STEVEHARVILLE
+export CX_ORACLE_PASSWORD=TopSecret
+export CX_ORACLE_TNSENTRY=TheDBname
+export CX_ORACLE_ARRAY_SIZE=1024
